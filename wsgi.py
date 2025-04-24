887271300bd652fdff32839ee3cd1aace4be6841
Davizoca123312 <davivbrr@gmail.com>
2025-04-24 13:18:48 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from main import app  # 'main' é o nome do seu arquivo atual (sem o .py)
+from app import app  # 'main' é o nome do seu arquivo atual (sem o .py)
 
 if __name__ == "__main__":
     app.run()
